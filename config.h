4d8e0d04a32a32d534b4bf589e3c2c2bf99dd4ce
salastro <salah2112004@gmail.com>
2025-01-30 17:45:58 +0200

    Update personal configuration

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER ""
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
 
 // Control whether blocks are clickable.
-#define CLICKABLE_BLOCKS 1
+#define CLICKABLE_BLOCKS 0
 
 // Control whether a leading delimiter should be prepended to the status.
 #define LEADING_DELIMITER 0
@@ -18,15 +18,15 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
-    X("", "sb-memory", 10, 4) \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
+    X("|\x01", "~/.config/dwmblocks/sb-date", 1, 0) \
+    X("|\x01", "~/.config/dwmblocks/sb-netbars", 2, 0) \
+    X(";|\x02", "~/.config/dwmblocks/sb-battery", 1, 0) \
+    X(">\x01", "~/.config/dwmblocks/sb-brightness", 0, 6) \
+    X(">\x02", "~/.config/dwmblocks/sb-volume", 0, 1) \
+    X(">\x01", "~/.config/dwmblocks/sb-xkeyboard", 0, 7) \
+    X(";|\x01", "~/.config/dwmblocks/sb-disk", 0, 0) \
+    X("<\x02", "~/.config/dwmblocks/sb-cpubars", 2, 0) \
+    X("<\x01", "~/.config/dwmblocks/sb-cputemp", 2, 0) \
+    X("<\x02", "~/.config/dwmblocks/sb-ram", 4, 0) \
 
 #endif  // CONFIG_H
